bfb24048795c3ae1106c3fe8da8d37d8377fd2a8
Talanoff <talanov.o@gmail.com>
2020-01-31 11:22:06 +0200

    FB pixel

--- wp-content/themes/slim/header.php
+++ wp-content/themes/slim/header.php
@@ -15,10 +15,27 @@
     <meta name="msapplication-config" content="<?php echo get_theme_file_uri('images/favicons/browserconfig.xml') ?>">
     <meta name="theme-color" content="#ffffff">
 
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '839849363141357');
+        fbq('track', 'PageView');
+    </script>
+    <!-- End Facebook Pixel Code -->
+
     <?php wp_head(); ?>
 </head>
 <body>
-
+<noscript>
+    <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=839849363141357&ev=PageView&noscript=1" />
+</noscript>
 <?php include(__DIR__ . '/includes/partials/svgs.php') ?>
 
 <!-- App-header -->
